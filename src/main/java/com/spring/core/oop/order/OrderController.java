da81fbec2f4af33a904293de1b3f595fe75e9f72
harangAi <87106103+harangAi@users.noreply.github.com>
2021-10-28 15:52:39 +0900

    [21.10.28] # Spring Core 2일차

---
+++ src/main/java/com/spring/core/oop/order/OrderController.java
@@ -0,0 +1,19 @@
+package com.spring.core.oop.order;
+
+import com.spring.core.oop.member.Member;
+import com.spring.core.oop.member.MemberRepository;
+import com.spring.core.oop.member.MemoryMemberRepository;
+
+// 책임 : 적당한 회원저장소에서 회원정보를 조회한 후 주문 정보를 생성해야 한다.
+public class OrderController {
+    private MemberRepository memberRepository;
+
+    public OrderController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    // 주문정보 생성
+    public Order createOrder(Long id) {
+        Member member = memberRepository.findById(id);
+        return new Order(id, "상품1", 5000);
+    }
+}
